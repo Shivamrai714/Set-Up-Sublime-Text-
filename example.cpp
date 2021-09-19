525511743587bad0c57322fb21c2001a7ae52808
Shivam Rai <72429552+Shivamrai714@users.noreply.github.com>
2021-09-20 01:10:30 +0530

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{                      // need to include these basic lines  & build system as "competitivep" of code in your compilation of the code.
+
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("inputf.in", "r", stdin);
+    // for writing output to output.txt
+    freopen("outputf.in", "w", stdout);
+#endif
+ 
+
+int a;
+cin>>a;
+cout<<a;
+cout<<"\n hi this is shivam rai"; 
+
+	return 0 ;
+
+
+
+
+// to set up 3 window , editor ,inputf.in,outputf.in
+    //1 goto view->layout->columns-3
+    //2 goto  view ->groups->max coulumn-2
+
+    //open respective files inputf.in,outputf.in
+
+//Also need to set up our own bulid system, refer youtube
+//in tools option
+
+}
